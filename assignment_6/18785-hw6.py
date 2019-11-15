3d2669e51304e1f94e384a81d175bc6b21bfc29c
ghostian <junxiao.guo@outlook.com>
2019-11-14 22:55:10 -0500

    HW6 Dev

--- assignment_6/18785-hw6.py
+++ assignment_6/18785-hw6.py
@@ -1,23 +1,26 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn import tree
-from sklearn.datasets import load_iris
-from sklearn import metrics
-from sklearn.externals.six import StringIO
-from IPython.display import Image
-from sklearn.tree import export_graphviz
 import pydotplus
-import matplotlib.image as mpimg
+import seaborn as sns
 import statsmodels.api as sm
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import StratifiedKFold, cross_val_score
-from sklearn.neighbors import KNeighborsClassifier
+from IPython.display import Image
 from matplotlib.colors import ListedColormap
-from sklearn import neighbors, datasets
+from sklearn import metrics
+from sklearn import tree
+from sklearn.externals.six import StringIO
+from sklearn.model_selection import cross_val_score
+from sklearn.feature_selection import SelectFromModel
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
+from sklearn.tree import export_graphviz
+from sklearn.linear_model import LassoCV
+from sklearn.neighbors import DistanceMetric
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_breast_cancer
+from sklearn.tree import DecisionTreeClassifier
 
 
 def data_preprocess():
@@ -39,28 +42,6 @@ def data_preprocess():
     return df2
 
 
-def q4():
-    df_red = pd.read_csv("winequality-red.csv", sep=';')
-    df_white = pd.read_csv("winequality-white.csv", sep=';')
-    print("---red---")
-    x_features = list(df_white.columns)
-    red_mean = list(df_red.mean())
-    white_mean = list(df_white.mean())
-    # ax = plt.subplot(111)
-    x = np.arange(len(x_features))
-    print(df_red.corr())
-    print(df_white.corr())
-    # print(x)
-    # print(red_mean)
-    # print(white_mean)
-    plt.bar(x + 0.2, red_mean, width=0.2, color='red', align='center',label='Red wine')
-    plt.bar(x, white_mean, width=0.2, color='grey', align='center',label='White wine')
-    print(x_features)
-    plt.xticks(x, x_features, rotation=90)
-    plt.legend()
-    plt.show()
-
-
 def encode_target(df, target_column):
     df_mod = df.copy()
     targets = df_mod[target_column].unique()
@@ -69,7 +50,7 @@ def encode_target(df, target_column):
     return (df_mod, targets)
 
 
-def q2_logt_reg():
+def p2_logt_reg():
     df = pd.read_csv("titanic3.csv")
     sex = pd.get_dummies(df['sex'], drop_first=True)
     pclass = pd.get_dummies(df['pclass'], drop_first=True)
@@ -93,31 +74,47 @@ def q2_logt_reg():
     print("Accuracy:", metrics.accuracy_score(binary_pred, list(Y)))
 
 
-def q2(df):
-    x_features = ['pclass', 'sex', 'age', 'fare', 'sibsp', 'embarked']
-    ytrain = df.survived
-    xtrain = df.drop(['survived'], axis=1)
-    clf = tree.DecisionTreeClassifier(max_depth=5)
-    clf = clf.fit(xtrain, ytrain)
-    print("finished")
-    reuslt = clf.predict(xtrain)
-    scores = cross_val_score(clf, xtrain, ytrain, cv=6)
-    print(scores)
-    print(scores.mean())
-    exit()
-    print("Accuracy:", metrics.accuracy_score(reuslt, list(ytrain)))
-    dot_data = StringIO()
-    export_graphviz(clf, out_file=dot_data,
-                    filled=True, rounded=True,
-                    special_characters=True, feature_names=x_features, class_names=['Not_survived', 'Survived'])
-    graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
-    graph.write_png('tree.png')
-    Image(graph.create_png())
+class p2():
+    def __init__(self, df):
+        self.x_features = ['pclass', 'sex', 'age', 'fare', 'sibsp', 'embarked']
+        self.ytrain = df.survived
+        self.xtrain = df.drop(['survived'], axis=1)
 
-    plt.show()
+    def q3(self):
+        clf = tree.DecisionTreeClassifier(max_depth=5)
+        clf = clf.fit(self.xtrain, self.ytrain)
+        print("finished")
+        reuslt = clf.predict(self.xtrain)
+        scores = cross_val_score(clf, self.xtrain, self.ytrain, cv=6)
+        print(scores)
+        print(scores.mean())
+        print("Accuracy:", metrics.accuracy_score(reuslt, list(self.ytrain)))
+
+        exit()
+        dot_data = StringIO()
+        export_graphviz(clf, out_file=dot_data,
+                        filled=True, rounded=True,
+                        special_characters=True, feature_names=self.x_features, class_names=['Not_survived', 'Survived'])
+        graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+        graph.write_png('tree.png')
+        Image(graph.create_png())
+        plt.show()
+
+    def q4(self):
+        clf = DecisionTreeClassifier(class_weight=None, criterion='gini', max_depth=5,
+                                     max_features=None, max_leaf_nodes=15, min_samples_leaf=3,
+                                     min_samples_split=2, min_weight_fraction_leaf=0.0,
+                                     presort=False, random_state=None, splitter='random')
+        clf = clf.fit(self.xtrain, self.ytrain)
+        print("finished")
+        reuslt = clf.predict(self.xtrain)
+        scores = cross_val_score(clf, self.xtrain, self.ytrain, cv=6)
+        print(scores)
+        print(scores.mean())
+        print("Accuracy:", metrics.accuracy_score(reuslt, list(self.ytrain)))
 
 
-def q3(df):
+def p3(df):
     gender = {'male': 1, 'female': 2}
     embark = {'S': 1, 'C': 2, 'Q': 3}
     df = pd.read_csv("titanic3.csv")
@@ -134,65 +131,190 @@ def q3(df):
     # x_features = ['pclass', 'sex', 'age', 'sibsp','fare','embarked']
     df = df[feature_cols]
     df2 = df
-    classifier = KNeighborsClassifier(n_neighbors=9)
+    classifier = KNeighborsClassifier(n_neighbors=6)
     ytrain = df2.survived
     xtrain = df2.drop(['survived'], axis=1)
     X, Y = xtrain.values, ytrain.values
-    scores = cross_val_score(classifier, xtrain, ytrain, cv=6)
+    scores = cross_val_score(classifier, X, Y, cv=6)
     print(scores.mean())
+
+    # Different Metrics
+    classifier = KNeighborsClassifier(n_neighbors=6, metric="euclidean")
+    scores = cross_val_score(classifier, X, Y, cv=6)
+    print("eucledian", scores.mean())
+    classifier = KNeighborsClassifier(n_neighbors=6, metric="manhattan")
+    scores = cross_val_score(classifier, X, Y, cv=6)
+    print("manhattan", scores.mean())
+
+    classifier = KNeighborsClassifier(n_neighbors=6, metric="chebyshev")
+    scores = cross_val_score(classifier, X, Y, cv=6)
+    print("chebyshev", scores.mean())
+
+    classifier = KNeighborsClassifier(n_neighbors=6, metric="minkowski")
+    scores = cross_val_score(classifier, X, Y, cv=6)
+    print("minkowski", scores.mean())
+
+    # classifier = KNeighborsClassifier(n_neighbors=6,metric="wminkowski")
+    # scores = cross_val_score(classifier, xtrain, ytrain, cv=6)
+    # print("wminkowski",scores.mean())
+
+    # classifier = KNeighborsClassifier(n_neighbors=6,metric="seuclidean")
+    # scores = cross_val_score(classifier, xtrain, ytrain, cv=6)
+    # print("seuclidean",scores.mean())
+
+    classifier = KNeighborsClassifier(n_neighbors=6, metric="mahalanobis", metric_params={'V': np.cov(X)})
+    scores = cross_val_score(classifier, X, Y, cv=6)
+    print("mahalanobis", scores.mean())
+
     exit()
-    X = X[:, :3]
-    # classifier.fit(xtrain, ytrain)
-    # result = classifier.predict(xtrain)
+    # X = X[:, :3]
+    # # classifier.fit(xtrain, ytrain)
+    # # result = classifier.predict(xtrain)
+    # # print("Accuracy:", metrics.accuracy_score(result, list(ytrain)))
+    # classifier.fit(X, Y)
+    # result = classifier.predict(X)
     # print("Accuracy:", metrics.accuracy_score(result, list(ytrain)))
-    classifier.fit(X, Y)
-    result = classifier.predict(X)
-    print("Accuracy:", metrics.accuracy_score(result, list(ytrain)))
-
-    n_neighbors = 5
-    h = 0.02
-    # Create color maps
-    cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
-    cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
-    # calculate min, max and limits
-    a_min, a_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    b_min, b_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    print(b_min, b_max)
-    c_min, c_max = X[:, 2].min() - 1, X[:, 2].max() + 1
-    print(c_min, c_max)
-    # d_min, d_max = X[:, 3].min() - 1, X[:, 3].max() + 1
-    # e_min, e_max = X[:, 4].min() - 1, X[:, 4].max() + 1
-    # f_min, f_max = X[:, 5].min() - 1, X[:, 5].max() + 1
-    xx, yy = np.meshgrid(np.arange(a_min, a_max, h),
-                         np.arange(b_min, b_max, h),
-                         np.arange(c_min, c_max, h),
-                         # np.arange(d_min, d_max, h),
-                         # np.arange(e_min, e_max, h),
-                         # np.arange(f_min, f_max, h),
-                         )
-
-    # predict class using data and kNN classifier
-    Z = classifier.predict(np.c_[xx.ravel(), yy.ravel()])
-
-    # Put the result into a color plot
-    Z = Z.reshape(xx.shape)
-    plt.figure()
-    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
-
-    # Plot also the training points
-    plt.scatter(X[:, 0], X[:, 1], c=Y, cmap=cmap_bold)
-    plt.xlim(xx.min(), xx.max())
-    plt.ylim(yy.min(), yy.max())
-    plt.title("3-Class classification (k = %i)" % (n_neighbors))
-    plt.show()
+    #
+    # n_neighbors = 5
+    # h = 0.02
+    # # Create color maps
+    # cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+    # cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+    # # calculate min, max and limits
+    # a_min, a_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    # b_min, b_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    # print(b_min, b_max)
+    # c_min, c_max = X[:, 2].min() - 1, X[:, 2].max() + 1
+    # print(c_min, c_max)
+    # # d_min, d_max = X[:, 3].min() - 1, X[:, 3].max() + 1
+    # # e_min, e_max = X[:, 4].min() - 1, X[:, 4].max() + 1
+    # # f_min, f_max = X[:, 5].min() - 1, X[:, 5].max() + 1
+    # xx, yy = np.meshgrid(np.arange(a_min, a_max, h),
+    #                      np.arange(b_min, b_max, h),
+    #                      np.arange(c_min, c_max, h),
+    #                      # np.arange(d_min, d_max, h),
+    #                      # np.arange(e_min, e_max, h),
+    #                      # np.arange(f_min, f_max, h),
+    #                      )
+    #
+    # # predict class using data and kNN classifier
+    # Z = classifier.predict(np.c_[xx.ravel(), yy.ravel()])
+    #
+    # # Put the result into a color plot
+    # Z = Z.reshape(xx.shape)
+    # plt.figure()
+    # plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+    #
+    # # Plot also the training points
+    # plt.scatter(X[:, 0], X[:, 1], c=Y, cmap=cmap_bold)
+    # plt.xlim(xx.min(), xx.max())
+    # plt.ylim(yy.min(), yy.max())
+    # plt.title("3-Class classification (k = %i)" % (n_neighbors))
+    # plt.show()
 
 
-# def q4():
+class p4():
+    def __init__(self):
+        self.df_red = pd.read_csv("winequality-red.csv", sep=';')
+        self.df_white = pd.read_csv("winequality-white.csv", sep=';')
+
+    def q1(self):
+        print("---red---")
+        x_features = list(self.df_white.columns)
+        red_mean = list(self.df_red.mean())
+        white_mean = list(self.df_white.mean())
+        # ax = plt.subplot(111)
+        x = np.arange(len(x_features))
+
+        # print(x)
+        # print(red_mean)
+        # print(white_mean)
+        plt.bar(x + 0.2, red_mean, width=0.2, color='red', align='center', label='Red wine')
+        plt.bar(x, white_mean, width=0.2, color='grey', align='center', label='White wine')
+        print(x_features)
+        plt.xticks(x, x_features, rotation=90)
+        plt.legend()
+        plt.show()
+
+    def q2(self):
+        cor_white = self.df_white.corr()
+        cor_red = self.df_red.corr()
+        # print(cor_red["quality"])
+        print(cor_white["quality"])
+        sns.heatmap(cor_white, annot=True, cmap=plt.cm.Reds)
+        plt.title("Correlation heatmap of white wine")
+        plt.show()
+        sns.heatmap(cor_red, annot=True, cmap=plt.cm.Reds)
+        plt.title("Correlation heatmap of red wine")
+        plt.show()
+
+    def q3(self):
+        Xw, Yw = self.df_white.drop(['quality'], axis=1), self.df_white['quality']
+        Xr, Yr = self.df_red.drop(['quality'], axis=1), self.df_red['quality']
+
+        def get_wine_info(x, y, kind):
+            clf = LassoCV(cv=5)
+            model = clf.fit(x, y)
+            mse = [i.mean() for i in model.mse_path_]
+            lambdas = model.alphas_
+            plt.plot(lambdas, mse)
+            plt.xlabel("lambda")
+            plt.ylabel("MSE")
+            plt.title("{} Wine MSE vs. Lambda".format(kind))
+            plt.show()
+            params = model.coef_
+            print(params)
+            sfm = SelectFromModel(clf, threshold=0.25)
+            sfm.fit(x, y)
+            n_features = sfm.transform(x).shape[1]
+            X_transform = sfm.transform(x)
+            print("----{} Wine-----".format(kind))
+            for i, j in zip(list(x.columns), sfm.get_support()):
+                print("{} : {}".format(i, j))
+
+        get_wine_info(Xw, Yw, "white")
+        get_wine_info(Xr, Yr, "Red")
+
+    def q4(self):
+        x, y = self.df_red[['volatile acidity', 'sulphates', 'alcohol']], self.df_red[['quality']]
+        y = y.values.ravel()
+        classifier = KNeighborsRegressor(n_neighbors=2, metric='euclidean')
+        model = classifier.fit(x, y)
+        print(model.get_params)
+        # print(model.kneighbors_graph(x))
+        scores = cross_val_score(classifier, x, y, cv=6)
+        y_pred = model.predict(x)
+        binary_pred = []
+        acc = metrics.accuracy_score(y_true=y, y_pred=[int(i) for i in y_pred])
+        print("Cross Validation Score:", scores.mean())
+        print("Accuracy: ", acc)
+        # classifier =
+
+    def q5(self):
+        x, y = self.df_red[['volatile acidity', 'sulphates', 'alcohol']], self.df_red[['quality']]
+        y = y.values.ravel()
+        classifier = KNeighborsRegressor(n_neighbors=5)
+        model = classifier.fit(x, y)
+        y_pred = model.predict(x)
+        mse = mean_squared_error(y, y_pred)
+        R_sqrd = r2_score(y, y_pred=y_pred)
+        print("MSE for KNN Regression: ", mse)
+        print("R^2 for KNN Regression: ", R_sqrd)
+        classifier = LinearRegression()
+        model = classifier.fit(x, y)
+        y_pred = model.predict(x)
+        mse = mean_squared_error(y, y_pred)
+        R_sqrd = r2_score(y, y_pred=y_pred)
+        print("MSE for Linear Regression: ", mse)
+        print("R^2 for Linear Regression: ", R_sqrd)
 
 
 if __name__ == "__main__":
-    # df = data_preprocess()
-    # q2(df)
-    # q2_logt_reg()
-    # q3(df)
-    q4()
+    df = data_preprocess()
+    p2(df).q4()
+    # p2_logt_reg()
+    # p3(df)
+    # p4().q2()
+    # p4().q3()
+    # p4().q4()
+    # p4().q5()
